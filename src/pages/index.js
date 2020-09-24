9a7607e2a708eff5eec265d4c61b9a995cccddbc
Andrew Efurhievwe <andyriles22@gmail.com>
2020-09-24 15:36:10 +0100

    debug

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export const query = graphql`
           category
           image {
             fixed(width: 50, height: 50) {
-              ...GatsbyContentfulFixed_tracedSVG
+              ...GatsbyContentfulFixed
             }
           }
         }
